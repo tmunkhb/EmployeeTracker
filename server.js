9e90bb9bdbf8c4adc2e28ea628877a1ffd5cb12b
tmunkhb <tmunkhb@gmail.com>
2022-10-30 18:52:43 -0400

    prompt user using inquirer

--- server.js
+++ server.js
@@ -10,3 +10,40 @@ const connection = mysql.createConnection({
     database: 'employees'
 });
 
+// Initiate promptUser
+const promptUser = () => {
+    inquirer.prompt({
+        type: 'list',
+        name: 'choices',
+        message: 'What do you want to do? (Please select one of the following)',
+        choices: ['View All Employees', 'View All Department', 'View All Roles', 'Add Employee', 'Add Role', 'Add Department', 'Update Employee', 'Finished!']
+    })
+    .then((data) => {
+        switch (data['choices']) {
+            case 'View All Employees':
+                viewEmployees();
+                break;
+            case 'View All Department':
+                viewDepartments();
+                break;
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Update Employee':
+                updateEmployee();
+                break;    
+            case 'Finished!':
+                break;
+        }
+    })
+};
+
