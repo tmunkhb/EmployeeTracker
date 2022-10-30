095f215f5d177ab95b2676906c2e926901d16805
tmunkhb <tmunkhb@gmail.com>
2022-10-30 18:53:54 -0400

    add employee/department/role functions

--- server.js
+++ server.js
@@ -84,3 +84,128 @@ const viewRoles = () => {
     })
 };
 
+// Add employee
+const addEmployee = () => {
+    connection.query('SELECT * FROM employee, roles', (err, results) => {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: 'firstName',
+                type: 'input',
+                message: 'What is the first name?',
+            },
+            {
+                name: 'lastName',
+                type: 'input',
+                message: 'What is the last name?',
+            },
+            {
+                name: 'role',
+                type: 'rawlist',
+                choices: () => {
+                    let empArr = [];
+                    for (let i = 0; i < results.length; i++) {
+                        empArr.push(results[i].title);
+                    }
+                    let newEmpArr = [...new Set(empArr)];
+                    return newEmpArr;
+                },
+                message: 'What is the role?'
+            }
+        ]).then(data => {
+            let chosenRole;
+
+            for (let i = 0; i < results.length; i++) {
+                if (results[i].title === data.role) {
+                    chosenRole = results[i];
+                }
+            }
+            connection.query('INSERT INTO employee SET ?',
+            {
+                first_name: data.firstName,
+                last_name: data.lastName,
+                role_id: chosenRole.id,
+            },
+            (err) => {
+                if (err) throw err;
+                console.log(`New employee ${data.firstName} ${data.lastName} has been added!`);
+                promptUser();
+            }
+          )
+        });
+    });
+};
+
+// Add role
+const addRole = () => {
+    connection.query('SELECT * FROM department', (err, results) => {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: 'title',
+                type: 'input',
+                message: 'Please enter title for the role',
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'Please enter salary for the role',
+            },
+            {
+                name: 'department',
+                type: 'rawlist',
+                choices: () => {
+                    let newRoleArr = [];
+                    for (let i = 0; i < results.length; i++) {
+                        newRoleArr.push(results[i].name);
+                    }
+                    return newRoleArr;
+                },
+                message: 'What department is this new role under?',
+            }
+        ]).then(data => {
+            let chosenDepartment;
+            for (let i = 0; i < results.length; i++) {
+                if (results[i].name === data.department) {
+                    chosenDepartment = results[i];
+                }
+            }
+            connection.query('INSERT INTO roles SET ?',
+            {
+                title: data.title,
+                salary: data.salary,
+                department_id: chosenDepartment.id
+            },
+            (err) => {
+                if (err) throw err;
+                console.log(`New role ${data.title} has been added!`);
+                promptUser();
+            })
+        });
+    });
+};
+
+// Add department function
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'text',
+            name: 'departmentName',
+            message: 'Please enter the name of the department'
+        },
+        ]).then(data => {
+            connection.query(`INSERT INTO department SET ?`,
+                {name: data.departmentName},
+                (err) => {
+                    if (err) throw err;
+                    console.log(`Added ${data.departmentName} department`);
+                    promptUser();
+                }
+            );
+        });
+};
+
+
+
+
+promptUser();
