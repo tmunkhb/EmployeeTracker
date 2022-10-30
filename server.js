95f1f51d6525d0eb4323e776bb3ca0569a6822de
tmunkhb <tmunkhb@gmail.com>
2022-10-30 18:53:18 -0400

    view employees/department/roles function

--- server.js
+++ server.js
@@ -47,3 +47,40 @@ const promptUser = () => {
     })
 };
 
+// View employees function
+const viewEmployees = () => {
+    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, roles.title AS role, roles.salary AS salary,
+    department.name AS department 
+    FROM employee
+    LEFT JOIN roles
+    ON employee.role_id = roles.id
+    LEFT JOIN department
+    ON roles.department_id = department.id`,
+    (err, results) => {
+        if (err) throw err;
+        console.log('Showing employees');
+        console.table(results);
+        promptUser();
+    })
+};
+
+// View departments function
+const viewDepartments = () => {
+    connection.query(`SELECT * FROM department`, (err, results) => {
+            if (err) throw err;
+            console.log(`Showing departments:`)
+            console.table(results);
+            promptUser();
+        });
+};
+
+// View employee roles function
+const viewRoles = () => {
+    connection.query(`SELECT * FROM roles`, (err, results) => {
+        if (err) throw err;
+        console.log(`Showing roles:`)
+        console.table(results);
+        promptUser();
+    })
+};
+
